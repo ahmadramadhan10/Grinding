c72068969e62c1fea46391149d51061db1ca8b1f
Ahmad Ramadhan <108397143+ahmadramadhan10@users.noreply.github.com>
2023-04-21 14:32:39 +0700

    Add files via upload

---
+++ Compfest11/C.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+const ll MAXN = 2e5 + 5;
+const ll mod = 1e9 + 7;
+const ll inf = 1e18;
+
+void solve(){
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(nullptr);
+    int TC = 1;
+    //cin >> TC;
+    while(TC--){
+        solve();
+    }
+}
