a464ff92bc3985c7b24742db8b32a39d60af7283
unknown <aaarama21@gmail.com>
2023-06-23 10:57:56 +0700

    push lazy

---
+++ Library/lazy_segtree.cpp
@@ -0,0 +1,68 @@
+template<int MAXN, typename T, const T& TERM>
+struct SegtreeLazy {
+    vector<T> t;
+    vector<int> lazy;
+    SegtreeLazy() {
+        t.assign(4 * MAXN, TERM);
+        lazy.assign(4 * MAXN, 0);
+    }
+
+    T combine(const T& x, const T& y) {
+        return (x > y ? x : y);
+    }
+
+    void build(int v, int s, int e, vector<T>& a) {
+        lazy[v] = 0;
+        if (s == e) {
+            t[v] = a[s];
+            return;
+        }
+        int mid = (s + e) >> 1;
+        build(v << 1, s, mid, a);
+        build(v << 1 | 1, mid + 1, e, a);
+        t[v] = combine(t[v << 1], t[v << 1 | 1]);
+    }
+
+    void upd(T& cur, T val) {
+        cur += val;
+    }
+
+    void push(int v, int s, int e) {
+        if (lazy[v] == 0 || s == e) return;
+        upd(t[v << 1], lazy[v]);
+        upd(t[v << 1 | 1], lazy[v]);
+        upd(lazy[v << 1], lazy[v]);
+        upd(lazy[v << 1 | 1], lazy[v]);
+        lazy[v] = 0;
+    }
+
+    void update(int v, int s, int e, int l, int r, T val) {
+        if (l > r) return; // out of the bond
+        if (l == s && e == r) {
+            upd(t[v], val);
+            upd(lazy[v], val);
+            return;
+        }
+        push(v, s, e);
+        int mid = (s + e) >> 1;
+        update(v << 1, s, mid, l, min(r, mid), val);
+        update(v << 1 | 1, mid + 1, e, max(l, mid + 1), r, val);
+        t[v] = combine(t[v << 1], t[v << 1 | 1]);
+    }
+
+    T get(int v, int s, int e, int l, int r) {
+        if (l > r)
+            return TERM;
+        if (l <= s && e <= r)
+            return t[v];
+        push(v, s, e);
+        int mid = (s + e) >> 1;
+        T p1 = get(v << 1, s, mid, l, min(r, mid)); // agar di luar range tidak di update
+        T p2 = get(v << 1 | 1, mid + 1, e, max(l, mid + 1), r); // agar di luar range tidak di update
+        return combine(p1, p2);
+    }
+};
+
+// const int N = 1e5;
+// const int INF = 2e9;
+// SegtreeLazy<N, int, -INF> sgt;
